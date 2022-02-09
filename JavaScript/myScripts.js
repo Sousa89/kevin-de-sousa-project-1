507b9b83d5891c4c617f40886716547b045cf3ab
Sousa89 <kevin.de.sousa89@gmail.com>
2022-02-09 06:32:18 -0500

    added comment descriptions to blog form. Worked on contact page form, couldnt get both forms to work at the same time

--- JavaScript/myScripts.js
+++ JavaScript/myScripts.js
@@ -2,66 +2,99 @@
 let commentForm = document.querySelector(`#commentForm`);
 console.log(commentForm)
 
-const fullName = commentForm.name
-const email = commentForm.email
-const message = commentForm.message
 
-console.log(email)
-console.log(commentForm)
+const commentFormName = commentForm.name
+const commentFormEmail = commentForm.email
+const commentFormMessage = commentForm.message
+
+// adding event listener on form when submitted with button
 commentForm = addEventListener(`submit`, function(e) {
   e.preventDefault();
 
-  // values from forms
-nameInput = fullName.value;
-console.log(nameInput)
-emailInput = email.value;
-console.log(emailInput)
-messageInput = message.value;
-console.log(messageInput)
-console.log(`form has been submitted`)
+  // .values from forms
+nameInput = commentFormName.value;
+emailInput = commentFormEmail.value;
+messageInput = commentFormMessage.value;
 
-// return `Thanks for leaving a comment ${nameInput}`,
-
-// document.querySelector(commentForm)
-
+// targeting the 
 const blogDiv = document.querySelector(`.blogComments`)
 
 const commentsBox = document.createElement(`div`)
 commentsBox.classList.add(`commentsBox`, `commentsBottom`)
 
-// for img container
+// To create img container in comments box
 const imgContainer = document.createElement(`div`)
+
+
 imgContainer.classList.add(`imgContainer`)
-// for img tag
+// for img tag in comments box
 const commentsImg = document.createElement(`img`)
+// for img src in comments box
 commentsImg.src = `https://placekitten.com/89/89`
 
-
+// creating the date and text portions within comments box
 const commentsText = document.createElement(`div`)
 commentsText.classList.add(`commentsText`)
 
+// for date/name within comments box
 const commentsDate = document.createElement(`p`)
+// for message within in comments box
 const commentsMessage = document.createElement(`p`)
 
+// for date to be added to commentsDate
 const date = new Date()
 
 blogDiv.appendChild(commentsBox)
-
+// appending image portion
 commentsBox.appendChild(imgContainer)
 imgContainer.appendChild(commentsImg)
-
+// appearing date and message portion
 commentsBox.appendChild(commentsText)
 commentsText.appendChild(commentsDate)
 commentsText.appendChild(commentsMessage)
 
+// user input for commentsDate displayed 
 commentsDate.textContent = `${date} by ${nameInput}`
+// user input commentsMessage displayed
 commentsMessage.textContent = messageInput
-// document.getElementsByClassName(`blogcomments`).appendChild
 
+// changing text in button to notify of submission
 document.querySelector(`button`).textContent = `Thanks for commenting ${nameInput}!`;
+
+// to reset form input after submission
+document.querySelector(`form`).reset();
+
+// just a little love
+alert(`I appreciate you! ❤️`);
 })
 
 
 
-// CONTACT PAGE 
+// // CONTACT PAGE 
 
+// let contactForm = document.querySelector(`#contactForm`);
+
+// const contactName = contactForm.name
+// const contactEmail = contactForm.email
+// const contactMessage = contactForm.message
+
+// console.log(email)
+// console.log(contactForm)
+// contactForm = addEventListener(`submit`, function(e) {
+//   e.preventDefault();
+
+//   // values from forms
+ 
+// nameInput = contactName.value;
+// console.log(nameInput);
+// emailInput = contactEmail.value;
+// console.log(emailInput);
+// messageInput = contactMessage.value;
+// console.log(messageInput);
+// console.log(`form has been submitted`);
+
+// alert(`Thank you! I'll get back to you as soon as I can!`);
+
+// document.querySelector(`button`).textContent = `Message Sent ${nameInput}!`;
+// document.querySelector(`form`).reset();
+// });
